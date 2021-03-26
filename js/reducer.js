4d889f04a4b7bd26c2fddfb61015d6c5eaf34288
Matthew Crooks <mfcrooks@gmail.com>
2021-03-26 20:49:51 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,23 +1,21 @@
 let state = {count: 0};
 
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+
+function render(){
+    document.body.textContent = state.count
 }
 
 function dispatch(action){
-  state = changeState(state,action)
-  render();
+  state = changeState(state, action)
+  render()
 }
 
-function render(){
-  document.body.textContent = state.count
-}
-
-dispatch({type: 'INCREASE_COUNT'})
-dispatch({type: 'INCREASE_COUNT'})
-dispatch({type: 'INCREASE_COUNT'})
+render()
